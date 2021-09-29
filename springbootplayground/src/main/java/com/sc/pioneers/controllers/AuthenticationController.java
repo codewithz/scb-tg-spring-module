babc43b125e66740de4da5ac2e7b57581d94d6da
Zartab Nakhwa <zartab@codewithz.com>
2021-09-29 11:59:26 +0530

    Logout Implemented

--- springbootplayground/src/main/java/com/sc/pioneers/controllers/AuthenticationController.java
+++ springbootplayground/src/main/java/com/sc/pioneers/controllers/AuthenticationController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sc.pioneers.config.APISuccessPayload;
@@ -55,6 +56,30 @@ public class AuthenticationController
 		
 		
 		ResponseEntity<APISuccessPayload> response=new ResponseEntity<APISuccessPayload>(payload,HttpStatus.CREATED);
+		return response;
+		
+		
+	}
+	@PostMapping("/appusers/logout")
+	public ResponseEntity<APISuccessPayload> logout(@RequestHeader("X-SCB-Username") String username,
+																@RequestHeader("X-SCB-Token") String token)
+	{
+		AppUserTokenVO tokenVO=new AppUserTokenVO();
+		tokenVO.setToken(token);
+		tokenVO.setUsername(username);
+		
+		authService.logout(tokenVO);
+		
+		APISuccessPayload payload=new APISuccessPayload();
+		HttpStatus status=HttpStatus.OK;
+		payload.setBody("Logout Successful");
+		payload.setMessage("Logout Successful");
+		payload.setStatus(status.value());
+		payload.setHttpStatus(String.valueOf(status));
+		payload.setSuccess(true);
+		payload.setException(false);
+		
+		ResponseEntity<APISuccessPayload> response=new ResponseEntity<APISuccessPayload>(payload,status);
 		return response;
 		
 		
